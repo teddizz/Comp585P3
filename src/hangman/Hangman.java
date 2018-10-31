4e624e8623bfe443190568314b5cda2748bea1f6
John Brehm <cooljohnny3@gmail.com>
2018-10-30 19:43:51 -0700

    REorganize

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -16,7 +16,7 @@ public class Hangman extends Application {
         loader.setController(new hangman.GameController(game));
         Parent root = loader.load();
         Scene scene = new Scene(root, 700, 780);
-        scene.getStylesheets().add(getClass().getResource("Hangman.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/Hangman.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
